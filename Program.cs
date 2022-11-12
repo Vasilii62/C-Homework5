f827c225191304b965bd2c212b3dc827d58929b0
email <vassili642@gmail.com>
2022-11-12 21:40:01 +0300

    Add task 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B (Math.Pow НЕ использовать)
+
+/*3, 5 -> 243 (3⁵)
+0, 0 - >1
+2, 4 -> 16*/
+
+/*int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for(int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+Console.Write("Enter the number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Enter the number B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("The answer is: " + exponentiation);*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+Console.WriteLine("Enter the number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int SumOfDigit(int numberA)
+{
+    
+} 
