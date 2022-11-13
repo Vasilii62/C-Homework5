7a6183433f3fd5ba0068642d78e4aaab68e1ba47
email <vassili642@gmail.com>
2022-11-13 19:37:47 +0300

    Add task 3

--- Program.cs
+++ Program.cs
@@ -56,4 +56,20 @@ Console.WriteLine("The sum of the digits in the number is: " + sumOfDigit);*/
 
 6, 1, 33 -> [6, 1, 33]*/
 
+Console.Write("Введите количество элементов массива:\t");
+int elementsCount = int.Parse(Console.ReadLine());
+int[] myArray = new int[elementsCount];
 
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}:\t");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Вывод массива:");
+for (int i = 0; i < myArray.Length; i++)
+{
+
+    Console.Write(myArray[i] + ", ");
+}
+//Console.ReadLine();
